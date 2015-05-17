898a0e9d5cc9347c705fcd839a199fa98b4a61e3
amelia98 <ameliaharris.student@wegc.school.nz>
2015-05-18 10:05:41 +1200

    Update baskeball.py

--- 1_4basketball.py
+++ 1_4basketball.py
@@ -12,14 +12,18 @@ class BasketballPlayer:
      """
      return self.standard_goals / self.games
 
-def display_average():
+ def display_average(self):
+     """
+     >>> b = BasketballPlayer("Karen", 100, 20)
+     >>> b.get_average()
+     The average goals scored per game by Karen is 5.0
+     """
+     print("The average goals scored per game by", self.name, "is", self.standard_goals / self.games)
+
+def best_player():
     best.append(ball1.get_average())
     best.append(ball2.get_average())
     best.append(ball3.get_average())
-    print("The average goals scored per game by", ball1.name, "is", ball1.get_average())
-    print("The average goals scored per game by", ball2.name, "is", ball2.get_average())
-    print("The average goals scored per game by", ball3.name, "is", ball3.get_average())
-
 
 # main routine
 if __name__ == "__main__":
@@ -29,5 +33,8 @@ if __name__ == "__main__":
  ball1 = BasketballPlayer("Karen", 100, 20)
  ball2 = BasketballPlayer("Perry", 24, 80)
  ball3 = BasketballPlayer("Megan", 55, 66)
- display_average()
+ ball1.display_average()
+ ball2.display_average()
+ ball3.display_average()
+ best_player()
  print(max(best))
